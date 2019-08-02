de1dd3f94fcaa2e3f5acbc3cd143bf7da1682a5e
Assuncao Digital <assuncaodigital@MacBook-Air-de-Assuncao.local>
2019-08-02 15:34:06 -0300

    ReplicaSet connection support

--- src/services/mongodb/index.js
+++ src/services/mongodb/index.js
@@ -4,7 +4,8 @@ const MongoClient = require('mongodb').MongoClient;
 const MongoDB = {
     connect: async () => {
         return new Promise((resolve, reject) => {
-            MongoClient.connect(`mongodb://${config.get().mongodb.username}:${config.get().mongodb.password}@${config.get().mongodb.host}/${config.get().mongodb.authenticationDatabase}`, { useNewUrlParser: true }, (err, socket) => {
+            let uri = `mongodb://${config.get().mongodb.username}:${config.get().mongodb.password}@${config.get().mongodb.host}/${config.get().mongodb.authenticationDatabase}?replicaSet=${config.get().mongodb.replicaSet}`;
+            MongoClient.connect(uri, { useNewUrlParser: true }, (err, socket) => {
                 if (err) return reject(err);
                 return resolve({ socket, db: socket.db(config.get().mongodb.database) });
             });
